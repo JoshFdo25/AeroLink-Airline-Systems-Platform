0c760b8879af7753522fa531ab42fdab0a550c78
JoshFdo25 <cb011604@students.apiit.lk>
2026-05-30 03:12:19 +0530

    fix: exclude root typescript files from build to fix main module path

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -6,6 +6,17 @@ module "eks" {
   cluster_endpoint_public_access = true
   vpc_id                         = var.vpc_id
   subnet_ids                     = var.private_subnets
+  node_security_group_additional_rules = {
+    ingress_istio_webhook = {
+      description                   = "Allow API server to reach Istio webhook"
+      protocol                      = "tcp"
+      from_port                     = 15017
+      to_port                       = 15017
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
+
   eks_managed_node_groups = {
     spot_nodes = {
       min_size       = 1
